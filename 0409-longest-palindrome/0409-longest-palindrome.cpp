5d39212cc36de4f254440131559af021956b1aea
Rajat Sudhir Patil <101641740+RajatSudhirPatil@users.noreply.github.com>
2023-01-14 09:27:06 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (52.18%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         unordered_map<char,int> m;
-        for(auto i:s){
-            m[i]++;
-        }
-        for(auto i:m){
-            cout<<i.first<<" "<<i.second<<endl;
-            
+        for(int i=0;i<s.size();i++){
+            m[s[i]]++;
         }
         int flag=0;
         int count=0;
         for(auto i:m){
             if(i.second%2==0) count+=i.second;
-            if(i.second%2!=0) {
+            else{
                 flag=1;
                 count+=i.second-1;
             }
         }
-        if(flag==1) count=count+1;
-       // count+=max;
-        return count;
+        int ans=0;
+        if(flag==0) ans=count;
+        else ans=count+1;
+        return ans;
     }
 };
