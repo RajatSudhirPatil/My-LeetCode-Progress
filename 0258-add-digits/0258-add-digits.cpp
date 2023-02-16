e7b7608503b9c4b7103fa5953ac067ec56bc1f45
Rajat Sudhir Patil <101641740+RajatSudhirPatil@users.noreply.github.com>
2023-02-16 19:57:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (65.20%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num<10) return num;
+        int sum=0;
+        while(num){
+            sum+=num%10;
+            num=num/10;
+        }
+        num=sum;
+        return addDigits(num);
+    }
+};
