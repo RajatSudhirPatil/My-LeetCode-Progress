b897be52556a37741cee21f649f2eb0c69b41bf9
Rajat Sudhir Patil <101641740+RajatSudhirPatil@users.noreply.github.com>
2023-02-23 02:15:57 +0530

    Time: 4 ms (39.40%), Space: 6.2 MB (40.17%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     string reversePrefix(string word, char ch) {
         auto pos=word.find(ch);
+        //npos is end of the string
         if(pos!=string::npos){
             reverse(word.begin(),word.begin()+pos+1);
         }
