826dbfaa86ae23e17bbe171ed83b6241a4192933
Rajat Sudhir Patil <101641740+RajatSudhirPatil@users.noreply.github.com>
2023-03-07 23:34:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (29.63%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int n) {
+        string s=to_string(n);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='6'){
+                s[i]='9';
+                break;
+            }
+        }
+        n=stoi(s);
+        return n;
+    }
+};
